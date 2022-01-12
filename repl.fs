88a5d2791db157bd70d6bdb0856d0e4a3920aaa9
Thomas Terry <thomas.terry@sierra-lang.net>
2022-01-12 10:05:55 -0600

    Add memory buffer creation

--- repl.fs
+++ repl.fs
@@ -5,20 +5,25 @@ require run.fs
 create temp-line max-line chars allot
 
 ( read up to max-line characters into the input buffer )
-: read-from-stdin-line ( -- buffer n flag? ) 
+: read-from-stdin-line ( -- ptr n ) 
 	temp-line max-line stdin read-line throw ( n flag )
 	( if the flag is false then we are at eof )
 	if 
 	else 
 		cr ." Exiting..." cr bye 
 	then ( n )
-	temp-line swap ( ptr n )
+	dup allocate throw ( n ptr2 )
+	2dup ( n ptr2 n ptr2 )
+	temp-line swap rot ( ptr1 ptr2 n )
+	cmove swap ( ptr n )
 ;
 
 : input-loop 
 begin 
 	." >>> " read-from-stdin-line ( ptr n )
-	run
+	swap dup rot ( ptr ptr n )
+	run ( ptr )
+	free throw
 again 
 ;
 
